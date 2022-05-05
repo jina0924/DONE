cf43ff9c0c7c5d01e34d74297fe8d3584375eac4
expertinpyo <ypd02363@naver.com>
2022-05-05 19:42:48 +0900

    updating calendar and stamps folders

--- stamps/urls.py
+++ stamps/urls.py
@@ -5,5 +5,8 @@ app_name = 'stamps'
 urlpatterns = [
     path('create/', views.create, name='create'),
     path('calendar/', views.calendar, name='calendar'),
+    path('calendars/', views.calendars, name='calendars'),
+    path('calendar/api/', views.api, name='api'),
     path('<int:goal_pk>/create_daily/', views.create_daily, name='create_daily'),
+
 ]
