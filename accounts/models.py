1b16bb653662fbb9e7cc85ea6bbb7f2eee05611c
jina jeong <jina092400@gmail.com>
2022-05-07 00:45:35 +0900

    Add profile image

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    image = models.ImageField(upload_to='images/', default='images/sample.jpg')
--- done/settings.py
+++ done/settings.py
@@ -146,3 +146,7 @@ STATICFILES_DIRS = (
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
